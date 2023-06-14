92b192d94a3a30298bc4d138fb8a1c9b2c9bb113
lisilya <lis.ilya@gmail.com>
2023-06-14 11:53:16 +0200

    add script.js

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const flashcardContainer = document.getElementById('flashcard-container');
+const newFlashcardBtn = document.getElementById('new-flashcard-btn');
+const newFlashcardModal = document.getElementById('new-flashcard-modal');
+const closeNewFlashcardModalBtn = document.getElementsByClassName('close')[0];
+const newFlashcardForm = document.getElementById('new-flashcard-form');
+
+function getFlashcards() {
+    fetch('http://localhost:8000/api/v1/flashcards/')
+        .then(response => response.json())
+        .then(data => {
+            for (let flashcard of data) {
+                const flashcardDiv = document.createElement('div');
+                flashcardDiv.classList.add('flashcard');
+                flashcardDiv.innerHTML = `
+                    <h2>${flashcard.question}</h2>
+                    <p>${flashcard.answer}</p>
+                `;
+                flashcardContainer.appendChild(flashcardDiv);
+            }
+        });
+}
+
+newFlashcardBtn.onclick = function() {
+    newFlashcardModal.style.display = 'block';
+}
+
+closeNewFlashcardModalBtn.onclick = function() {
+    newFlashcardModal.style.display = 'none';
+}
+
+newFlashcardForm.onsubmit = function(e) {
+    e.preventDefault();
+    const question = document.getElementById('question').value;
+    const answer = document.getElementById('answer').value;
+
+    fetch('http://localhost:8000/api/v1/flashcards/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            question: question,
+            answer: answer,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const flashcardDiv = document.createElement('div');
+        flashcardDiv.classList.add('flashcard');
+        flashcardDiv.innerHTML = `
+            <h2>${data.question}</h2>
+            <p>${data.answer}</p>
+        `;
+        flashcardContainer.appendChild(flashcardDiv);
+        newFlashcardModal.style.display = 'none';
+    });
+}
+
+getFlashcards();
