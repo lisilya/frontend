c389e7f4c2a5013268fc2274ce0938a2807c17ad
lisilya <lis.ilya@gmail.com>
2023-06-14 15:56:40 +0200

    Added start-learning feature

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const newFlashcardBtn = document.getElementById('new-flashcard-btn');
 const newFlashcardModal = document.getElementById('new-flashcard-modal');
 const closeNewFlashcardModalBtn = document.getElementsByClassName('close')[0];
 const newFlashcardForm = document.getElementById('new-flashcard-form');
+const startLearningBtn = document.getElementById('start-learning-btn');
 
 function getFlashcards() {
     fetch('http://localhost:8000/api/v1/flashcards/')
@@ -27,9 +28,14 @@ function getFlashcards() {
                     });
                 };
                 flashcardDiv.appendChild(deleteBtn);
+                flashcardDiv.onclick = function() {
+                    flashcardDiv.classList.toggle('show');
+                };
             }
         });
 }
+
+getFlashcards();
 
 newFlashcardBtn.onclick = function() {
     newFlashcardModal.style.display = 'block';
@@ -99,4 +105,100 @@ function createFlashcardsFromUrl(url) {
     });
 }
 
-getFlashcards();
+function startLearning() {
+    fetch('http://localhost:8000/api/v1/flashcards/?ordering=knowledgeLevel')
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < Math.min(data.length, 10); i++) {
+                let flashcard = data[i];
+                const flashcardDiv = document.createElement('div');
+                flashcardDiv.classList.add('flashcard');
+                flashcardDiv.innerHTML = `
+                    <h2>${flashcard.question}</h2>
+                    <p class="answer">${flashcard.answer}</p>
+                    <button class="known">I knew this</button>
+                    <button class="unknown">I didn't know this</button>
+                `;
+                flashcardDiv.onclick = function() {
+                    flashcardDiv.classList.toggle('show');
+                };
+                flashcardContainer.appendChild(flashcardDiv);
+                flashcardDiv.querySelector('.known').onclick = function() {
+                    fetch(`http://localhost:8000/api/v1/flashcards/${flashcard.id}/`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            correct_answers: flashcard.correct_answers + 1,
+                        }),
+                    });
+                };
+            }
+        });
+}
+
+// Start learning function
+
+let learningFlashcards = [];
+let currentFlashcardIndex = 0;
+
+function startLearning() {
+    fetch('http://localhost:8000/api/v1/flashcards/?ordering=correct_answers')
+        .then(response => response.json())
+        .then(data => {
+            learningFlashcards = data.slice(0, 10);
+            displayFlashcard(learningFlashcards[currentFlashcardIndex]);
+        });
+}
+
+function displayFlashcard(flashcard) {
+    flashcardContainer.innerHTML = '';
+    const flashcardDiv = document.createElement('div');
+    flashcardDiv.classList.add('flashcard');
+    flashcardDiv.innerHTML = `
+        <h2>${flashcard.question}</h2>
+        <p style="display: none;">${flashcard.answer}</p>
+        <button class="known">I knew this</button>
+        <button class="unknown">I didn't know this</button>
+    `;
+    flashcardContainer.appendChild(flashcardDiv);
+
+    flashcardDiv.onclick = function() {
+        flashcardDiv.getElementsByTagName('p')[0].style.display = 'block';
+    }
+
+    flashcardDiv.querySelector('.known').onclick = function(e) {
+        e.stopPropagation();
+        fetch(`http://localhost:8000/api/v1/flashcards/${flashcard.id}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                correct_answers: flashcard.correct_answers + 1,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            currentFlashcardIndex++;
+            if (currentFlashcardIndex < learningFlashcards.length) {
+                displayFlashcard(learningFlashcards[currentFlashcardIndex]);
+            } else {
+                flashcardContainer.innerHTML = '<p>You have finished this learning session.</p>';
+            }
+        });
+    };
+
+    flashcardDiv.querySelector('.unknown').onclick = function(e) {
+        e.stopPropagation();
+        currentFlashcardIndex++;
+        if (currentFlashcardIndex < learningFlashcards.length) {
+            displayFlashcard(learningFlashcards[currentFlashcardIndex]);
+        } else {
+            flashcardContainer.innerHTML = '<p>You have finished this learning session.</p>';
+        }
+    };
+}
+
+startLearningBtn.onclick = startLearning;
